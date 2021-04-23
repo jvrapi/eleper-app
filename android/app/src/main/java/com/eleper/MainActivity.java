b5cbe714120eafdddf07c130e263cb8133a7da28
João Vitor Santos <joaooviitorr@hotmail.com>
2021-04-23 19:43:56 -0300

    change de app name

--- android/app/src/main/java/com/eleper/MainActivity.java
+++ android/app/src/main/java/com/eleper/MainActivity.java
@@ -1,4 +1,4 @@
-package com.phr;
+package com.eleper;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "PHR";
+    return "eleper";
   }
 }
